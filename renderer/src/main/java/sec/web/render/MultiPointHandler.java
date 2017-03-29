c1502c9ec609539c6ae674000c24abe85b46e7a2
Michael Spinelli <michael.spinelli@us.army.mil>
2017-03-29 10:25:57 -0400

    -removed style id from output kml as it's never referenced.

--- renderer/src/main/java/sec/web/render/MultiPointHandler.java
+++ renderer/src/main/java/sec/web/render/MultiPointHandler.java
@@ -2704,7 +2704,7 @@ public class MultiPointHandler {
         String googleLineColor = null;
         String googleFillColor = null;
 
-        String lineStyleId = "lineColor";
+        //String lineStyleId = "lineColor";
 
         BasicStroke stroke = null;
         int lineWidth = 4;
@@ -2716,7 +2716,8 @@ public class MultiPointHandler {
 
         kml.append("<Placemark>");//("<Placemark id=\"" + id + "_mg" + "\">");
         kml.append("<description>" + cdataStart + "<b>" + name + "</b><br/>" + "\n" + description + cdataEnd + "</description>");
-        kml.append("<Style id=\"" + lineStyleId + "\">");
+        //kml.append("<Style id=\"" + lineStyleId + "\">");
+        kml.append("<Style>");
 
         lineColor = shapeInfo.getLineColor();
         if (lineColor != null) {
